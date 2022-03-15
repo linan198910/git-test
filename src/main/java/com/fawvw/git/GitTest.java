ab18009cea3510a45e2ab726a68a5a763f9b2c4e
an.lin <lin.an.ok@qq.com>
2022-03-15 18:50:30 +0800

    first commit

---
+++ src/main/java/com/fawvw/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.fawvw.git;
+
+/**
+ * @author an.lin
+ * @date 2022/3/15
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        System.out.println("main");
+    }
+}
