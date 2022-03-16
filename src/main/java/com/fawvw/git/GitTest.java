947121eeef79f557a3834a321fbb87e2135c535c
an.lin <lin.an.ok@qq.com>
2022-03-16 10:16:39 +0800

    master commit 4

--- src/main/java/com/fawvw/git/GitTest.java
+++ src/main/java/com/fawvw/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello github2");
         System.out.println("hot-fix");
+        System.out.println("hot-fix master");
     }
 }
